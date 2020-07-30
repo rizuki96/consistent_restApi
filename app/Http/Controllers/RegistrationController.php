bf86f362de2d377e74e9f1aa998db5dc8250dc9a
mrizkyfirmansyah <rizkyfirmansyah967@gmail.com>
2020-07-30 16:39:53 +0700

    create RegistrationController

---
+++ app/Http/Controllers/RegistrationController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RegistrationRequest;
+use App\User;
+
+class RegistrationController extends Controller
+{
+    public function register(RegistrationRequest $request)
+    {
+        User::create($request->getAttributes());
+
+        return $this->respondWithMessage('User successfully created');
+    }
+}
