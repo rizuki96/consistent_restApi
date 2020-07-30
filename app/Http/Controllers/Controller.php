ca89c9e42c81103ec9a160e23917341fa3b9c3ec
mrizkyfirmansyah <rizkyfirmansyah967@gmail.com>
2020-07-30 15:12:30 +0700

    add marcin-orlowski/laravel-api-response-builder composer package and create function for response

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,39 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use MarcinOrlowski\ResponseBuilder\ResponseBuilder as RB;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function respond($data, $message = null)
+    {
+        return RB::asSuccess()->withData($data)->withMessage($message)->build();
+    }
+
+    public function respondWithMessage($message)
+    {
+        return RB::asSuccess()->withMessage($message)->build();
+    }
+
+    public function respondWithError($api_code, $http_code)
+    {
+        return RB::asError($api_code)->withHttpCode($http_code)->build();
+    }
+
+    public function respondBadRequest($api_code)
+    {
+        return $this->respondWithError($api_code, 400);
+    }
+
+    public function respondUnAuthenticated($api_code)
+    {
+        return $this->respondWithError($api_code, 401);
+    }
+
+    public function respondNotFound($api_code)
+    {
+        return $this->respondWithError($api_code, 404);
+    }
 }
